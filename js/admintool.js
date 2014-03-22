4598a162093ebe57ef35fbecb8c5a7df7cd8f2fa
crash-horror <vasskazis@gmail.com>
2014-03-22 19:17:57 +0200

    373 changes to rc1

--- js/admintool.js
+++ js/admintool.js
@@ -1,4 +1,4 @@
-var DEFAULT_SERVER = "http://dcs-mission-planner.combined-ops-group.com:8080";
+var DEFAULT_SERVER = "http://147.102.19.134:8080";
 var USE_SSL = false; // whether to use ws: / http: or wss: / https: schemes
 var ADMIN_URI = null;
 var WEBSOCKET_URI = null;
@@ -18,7 +18,7 @@ function set_status(text) {
 
 $(function() {
 	$("#server-input").val(DEFAULT_SERVER);
-	
+
 	Lua.initialize();
 	load_table_show();
 	Lua.inject(function(k, v) { ipc[k] = v; return []; }, "ipc_set");
@@ -32,18 +32,18 @@ $(function() {
 
 	$("#noscript").hide();
 	$("#task_choice").show();
-	
+
 	$("#create_instance_choice").click(function() {
 		$("#task_choice").hide();
 		$("#create_input_step1").show();
 	});
-	
+
 	$("#file-input").change(function() {
 		var server_uri = URI($("#server-input").val());
 		USE_SSL = !!(server_uri.scheme() == "https");
 		WEBSOCKET_URI = new URI().host(server_uri.host()).scheme(USE_SSL ? "wss" : "ws");
 		WEBSOCKET_URI.path("/websocket/");
-		
+
 		$("#create_input_step1").hide();
 		set_status("hashing file...");
 		var fr = new FileReader();
@@ -86,14 +86,14 @@ $(function() {
 				}
 				ws.onmessage = function(msg) {
 					ws.close();
-					
+
 					set_status("processing response...");
 					var data = JSON.parse(msg.data);
 					var u = new URI().scheme(USE_SSL ? "https" : "http").host(new URI($("#server-input").val()).host()).path("/").query({"instance_id": data.instance_id});
 					u.username("admin").password(data.admin_pw);
 					$("#admin-url-input").val(u.toString());
 					$("#connect_existing_form").submit();
-					
+
 					set_status("instance created.");
 				}
 			},
@@ -102,8 +102,8 @@ $(function() {
 			"utf-8");
 		});
 	}
-	
-	
+
+
 	$("#existing_instance_choice").click(function() {
 		$("#task_choice").hide();
 		$("#connect_existing_step1").show();
@@ -128,13 +128,13 @@ $(function() {
 			fr.readAsArrayBuffer(document.getElementById("file-input").files[0]);
 		});
 		if (!document.getElementById("file-input").files.length) $("#save-mission-button").attr("disabled", true);
-		
+
 		$("#connect_existing_step1").hide();
 		ADMIN_URI = new URI($("#admin-url-input").val());
 		USE_SSL = !!(ADMIN_URI.scheme() == "https");
 		WEBSOCKET_URI = new URI();
 		WEBSOCKET_URI.scheme(USE_SSL ? "wss" : "ws").host(ADMIN_URI.host()).path("/websocket/");
-		
+
 		set_status("opening websocket...");
 		var ws = new WebSocket(WEBSOCKET_URI.toString());
 		ws.onopen = function() {
@@ -152,17 +152,17 @@ $(function() {
 				alert("Error: "+data.error_msg);
 				return;
 			}
-			
+
 			var u = new URI().scheme(USE_SSL ? "https" : "http").host(ADMIN_URI.host()).path("/").query({"instance_id": data.instance_id});
-			
+
 			u.username("admin").password(data.admin_pw);
 			$("#admin-url").text(u.toString());
 			$("#admin-url").attr("href", u.toString());
-			
+
 			u.username("red").password(data.red_pw);
 			$("#red-url").text(u.toString());
 			$("#red-url").attr("href", u.toString());
-			
+
 			u.username("blue").password(data.blue_pw);
 			$("#blue-url").text(u.toString());
 			$("#blue-url").attr("href", u.toString());
@@ -170,23 +170,23 @@ $(function() {
 			u.username("red").password(data.red_spectator_pw);
 			$("#red-spectator-url").text(u.toString());
 			$("#red-spectator-url").attr("href", u.toString());
-			
+
 			u.username("blue").password(data.blue_spectator_pw);
 			$("#blue-spectator-url").text(u.toString());
 			$("#blue-spectator-url").attr("href", u.toString());
-			
+
 			$("#instance-id-td").text(data.instance_id);
-			
+
 			$("#connect_existing_step2").show();
 			set_status("");
 		}
 	});
-	
+
 	$("#red-url, #blue-url, #admin-url").click(function(e) {
 		alert("Right-click the link and copy the URL to your clipboard!");
 		return false;
 	});
-	
+
 	$("#save-mission-button").click(function(evt) {
 		var file = document.getElementById("file-input").files[0];
 		var filename = document.getElementById("file-input").files[0].name;
@@ -195,10 +195,10 @@ $(function() {
 			zipfs.find("mission").getText(function(luaCode) {
 				set_status("loading mission...");
 				Lua.exec(luaCode);
-				
+
 				zipfs.importBlob(file, function() {
 					zipfs.remove(zipfs.find("mission"));
-					
+
 					set_status("opening websocket...");
 					var ws = new WebSocket(WEBSOCKET_URI.toString());
 					ws.onopen = function() {
@@ -227,11 +227,11 @@ $(function() {
 						setTimeout(function() {
 							Lua.exec("ipc_set('mission_str', 'mission = ' .. DUMP.tostring(mission, 'mission'))")
 							var mission_str = ipc.mission_str;
-							
+
 							setTimeout(function() {
 								set_status("processing response [compressing mission file]...");
 								zipfs.root.addText("mission", mission_str);
-								
+
 								set_status("opening download dialog.");
 								zipfs.exportBlob(function(blob) {
 									window.theblob = blob; // keep a reference
@@ -247,14 +247,14 @@ $(function() {
 						}, 20);
 					}
 				});
-					
+
 			},
 			null,
 			true,
 			"utf-8");
 		});
 	});
-	
+
 	$("#upload_mission_state_button").click(function(evt) {
 		var file = document.getElementById("state-input").files[0];
 		var filename = document.getElementById("state-input").files[0].name;
@@ -286,7 +286,7 @@ $(function() {
 				}
 		}
 	});
-	
+
 	$("#upload_livery_list_button").click(function(evt) {
 		var file = document.getElementById("livery-list-input").files[0];
 		var filename = document.getElementById("livery-list-input").files[0].name;
@@ -298,13 +298,13 @@ $(function() {
 			var ws = new WebSocket(WEBSOCKET_URI.toString());
 			ws.onopen = function() {
 				set_status("uploading livery list...");
-				
+
 				var input_liveries = JSON.parse(text);
 				var liveries = {};
 				$.each(input_liveries, function(key, value) {
 					liveries[key.toUpperCase()] = value;
 				});
-				
+
 				var request = { request_id: 1,
 								request: "set_liveries",
 								admin_pw: ADMIN_URI.password(),
@@ -325,6 +325,6 @@ $(function() {
 			}
 		}
 	});
-	
-	
+
+
 });
